3d183c61b0a214f004325549bba795db1cc56de2
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-05-07 19:50:17 +0200

    made stuff

--- js/script.js
+++ js/script.js
@@ -1,40 +1,53 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".api-container");
 
-
+const url = "https://jorgeneksamen2022.online/wp-json/wp/v2/posts/";
+const imageUrl = "https://jorgeneksamen2022.online/wp-json/wp/v2/media/";
 
 async function getApi() {
-    try {
-        const response = await fetch ("https://jorgeneksamen2022.online/wp-json/wp/v2/posts/");
+  try {
+    const response = await fetch(url);
 
-        const json = await response.json();
-        console.log(json)
-        
+    const data = await response.json();
+    console.log(data);
 
-        for (let i = 0; i < json.length; i++){
-            console.log(json[i])
-            const headline = (json[i].title.rendered)
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i]);
 
+      const headline = data[i].title.rendered;
 
-            container.innerHTML += `
-                                     <h2>${headline}</h2>`
-        }
-        
-        console.log(json[0])
-        const title = (json[0].title)
-        console.log(json[0].content.rendered)
-
-
-
-        
-        
+      container.innerHTML += `      <a href="spesific-projects.html"
+                                     <h2 class=project>${headline}</h2>
+                                     <img src=${data[i].featured_media_src_url}>`;
     }
-
-    catch(err){
-        console.log(err);
-    }
-
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 getApi();
 
+// async function getImage() {
+//   try {
+//     const response = await fetch(imageUrl);
+//     const imageData = await response.json();
 
+//     for (let i = 0; i < imageData.length; i++) {
+//       const image = imageData[i].guid.rendered;
+//       container.innerHTML += `<img class="project" src="${image}">`;
+//     }
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// getImage();
+
+// // function createNode(getApi, callImage) {
+// //   return `
+// //             <div>
+// //                 <h1>${title}</h1>
+// //                 <img src="${image}"
+// //             </div>`;
+// // }
+
+// // createNode();
