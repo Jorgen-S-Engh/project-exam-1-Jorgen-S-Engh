b4037fee2cfd8da477e5d46c35ca865cf6b3509e
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-05-12 14:47:03 +0200

    woring on slider

--- js/script.js
+++ js/script.js
@@ -8,8 +8,6 @@ async function getApi() {
   // console.log(data);
 
   for (let i = 0; i < data.length; i++) {
-    
-
     container.innerHTML += `
     <a class="project-items" href="spesific-projects.html?id=${data[i].id}">
         <h2>${data[i].title.rendered}</h2>
@@ -20,7 +18,6 @@ async function getApi() {
 }
 
 getApi();
-
 
 // video slider
 
@@ -44,31 +41,51 @@ function videoSlide() {
 
 videoSlide();
 
-
 //hamburger menu
 const hamburgerMenu = document.querySelector(".fa-bars");
 const exitMenu = document.querySelector(".fa-xmark");
 const menu = document.querySelector(".menu");
 
-
-hamburgerMenu.addEventListener("click", function(){ 
-  if (menu.style.display === "block"){
-    menu.style.display ="none"
+hamburgerMenu.addEventListener("click", function () {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
   }
-  else{
-    menu.style.display ="block"
+});
+
+exitMenu.addEventListener("click", function () {
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+    exitMenu.style.display = "block";
+  } else {
+    menu.style.display = "none";
   }
-}); 
+});
 
-exitMenu.addEventListener("click", function(){ 
-  if (menu.style.display === "none"){
-    menu.style.display ="block"
-    exitMenu.style.display ="block"
+// Karusell
 
-  }
-  else{
-    menu.style.display ="none"
-  }
-}); 
+// let slideIndex = 1;
+// showSlides(slideIndex);
 
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides((slideIndex += n));
+// }
 
+// function showSlides(n) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+
+//   slides[slideIndex - 1].style.display = "block";
+// }
