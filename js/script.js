529826ad7dfb4de48c4bab031bde0844b519451c
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-05-18 11:21:37 +0200

    made slider work

--- js/script.js
+++ js/script.js
@@ -1,25 +1,7 @@
 const container = document.querySelector(".api-container");
 const slideContainer = document.querySelector(".slide-container");
 
-const buttons = document.querySelectorAll("[data-carousel-button]");
-const slide = document.querySelector("[data-slides]");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]");
-
-    const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
-
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
-  });
-});
 
 const url = "https://jorgeneksamen2022.online/wp-json/wp/v2/posts/";
 
@@ -35,14 +17,12 @@ async function getApi() {
     //     <p>${data[i].excerpt.rendered}</p>
     // </a>`;
     slideContainer.innerHTML += `
-    <li class="slide">
+    <li class="slide" ${i === 0 ? "data-active='true'" : ""}>
       <img src="${data[i].featured_media_src_url}" alt="Nature Image #2">
-    </li>
-    <li class="slide">
-      <img src="${data[i].featured_media_src_url}" alt="Nature Image #3">
-    </li>`;
+    </li>`
 
-    // container.innerHTML += `
+
+    // container.innerHTML += 
     // <a class="project-items" href="spesific-projects.html?id=${data[i].id}">
     //     <h2>${data[i].title.rendered}</h2>
     //     <img src=${data[i].featured_media_src_url}>
@@ -99,32 +79,52 @@ exitMenu.addEventListener("click", () => {
 
 // KARUSELL
 
-const carouselImages = document.querySelector(".carousel__images");
-const carouselButtons = document.querySelectorAll(".carousel__button");
-const numberOfImages = document.querySelectorAll(
-  ".carousel__images img"
-).length;
-let imageIndex = 1;
-let translateX = 0;
+// const carouselImages = document.querySelector(".carousel__images");
+// const carouselButtons = document.querySelectorAll(".carousel__button");
+// const numberOfImages = document.querySelectorAll(
+//   ".carousel__images img"
+// ).length;
+// let imageIndex = 1;
+// let translateX = 0;
 
-console.log(numberOfImages);
+// console.log(numberOfImages);
 
-carouselButtons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    if (event.target.id === "previous") {
-      if (imageIndex !== 1) {
-        imageIndex--;
-        translateX += 400;
-      }
-    } else {
-      if (imageIndex !== numberOfImages) {
-        imageIndex++;
-        translateX -= 400;
-      }
-    }
+// carouselButtons.forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//     if (event.target.id === "previous") {
+//       if (imageIndex !== 1) {
+//         imageIndex--;
+//         translateX += 400;
+//       }
+//     } else {
+//       if (imageIndex !== numberOfImages) {
+//         imageIndex++;
+//         translateX -= 400;
+//       }
+//     }
 
-    carouselImages.style.transform = `translateX(${translateX}px)`;
-  });
-});
+//     carouselImages.style.transform = `translateX(${translateX}px)`;
+//   });
+// });
 
 // KARUSELL 2
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+const slide = document.querySelector("[data-slides]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
