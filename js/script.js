6dbfa2824a0e1f04a51c1edd05e3974a1fbc8f28
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-05-20 12:48:45 +0200

    finished contact.html

--- js/script.js
+++ js/script.js
@@ -52,27 +52,7 @@ function videoSlide() {
 
 videoSlide();
 
-//hamburger menu
-const hamburgerMenu = document.querySelector(".fa-bars");
-const exitMenu = document.querySelector(".fa-xmark");
-const menu = document.querySelector(".menu");
 
-hamburgerMenu.addEventListener("click", () => {
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
-});
-
-exitMenu.addEventListener("click", () => {
-  if (menu.style.display === "none") {
-    menu.style.display = "block";
-    exitMenu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
-});
 
 // KARUSELL
 
