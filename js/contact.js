564e5bdc93593c42175745e20a81d50a6649e5f3
Jorgen-S-Engh <55982088+Jorgen-S-Engh@users.noreply.github.com>
2022-05-26 21:28:43 +0200

    still contact

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,25 @@
-// function changeLabel() {
-//   document.querySelector(".tema-field").value = "";
-//   document.querySelector(".tema-field").placeholder = "new text for email";
-// }
+// document.querySelector(".tema-field").placeholder = "new text for tema";
 
-// changeLabel();
-document.querySelector(".tema-field").value = "";
-document.querySelector(".tema-field").placeholder = "new text for email";
+// element.classList.add("otherclass");
+const name = document.querySelector(".name-field");
+const tema = document.querySelector(".tema-field");
+const form = document.querySelector("form");
+
+function validateForm() {
+  event.preventDefault();
+
+  if (name.value.trim().length <= 6) {
+    name.classList.add("error-form");
+    // document.querySelector(".name-field").value = "";
+  } else {
+    name.classList.add("correct-form");
+  }
+
+  if (tema.value.length > 15) {
+    name.classList.add("error-form");
+    document.querySelector(".tema-field").value = "";
+  }
+}
+
+// https://www.youtube.com/watch?v=rsd4FNGTRBw
+// https://codepen.io/FlorinPop17/pen/OJJKQeK
